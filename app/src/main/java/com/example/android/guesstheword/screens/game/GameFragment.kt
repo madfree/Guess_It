48125277ad42e0729ee1831f5f2b9b3785f1cca3
madfree <matzefrei@gmail.com>
2020-08-23 18:13:44 +0200

    feat: Adding buzzer for correct answer, game finish and count down close to end

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -16,12 +16,16 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -64,6 +68,13 @@ class GameFragment : Fragment() {
             }
         })
 
+        viewModel.eventBuzz.observe(viewLifecycleOwner, Observer { hasBuzz ->
+            if (hasBuzz != GameViewModel.BuzzType.NO_BUZZ) {
+                buzz(hasBuzz.pattern)
+                viewModel.onBuzzComplete()
+            }
+        })
+
         return binding.root
     }
 
@@ -75,5 +86,20 @@ class GameFragment : Fragment() {
         val action = GameFragmentDirections.actionGameToScore(currentScore)
         findNavController(this).navigate(action)
         viewModel.onGameFinishComplete()
+    }
+
+    private fun buzz(pattern: LongArray) {
+        val buzzer = activity?.getSystemService<Vibrator>()
+
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+                Log.i("GameFragment", "Vibrating")
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+                Log.i("GameFragment", "Vibrating")
+            }
+        }
     }
 }
